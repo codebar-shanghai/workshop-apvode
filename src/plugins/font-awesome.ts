4909962040cfb3d207030360d99726c5f55a7fbc
IronBlood <aplshenyang@gmail.com>
2024-03-18 18:57:15 +0800

    import font awesome

---
+++ src/plugins/font-awesome.ts
@@ -0,0 +1,19 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import {
+	faTimeline,
+} from "@fortawesome/free-solid-svg-icons";
+
+import {
+	faLightbulb,
+} from "@fortawesome/free-regular-svg-icons";
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faLightbulb);
+library.add(faTimeline);
+
+export {
+	FontAwesomeIcon,
+}
+
