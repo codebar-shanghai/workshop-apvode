1d6f10bdf846b7e9440e98822a21661491814637
IronBlood <aplshenyang@gmail.com>
2024-03-28 21:23:47 +0800

    to manage hooks when changing slides

---
+++ src/lib/ResourceManager.ts
@@ -0,0 +1,24 @@
+type Callback = () => void;
+
+export const constructors = new Map<string, Callback>();
+
+export const destructors = new Map<string, Callback>();
+
+export function register_resource(cfg: {
+	id: string;
+	constructor: Callback;
+	destructor: Callback;
+}) {
+	if (constructors.has(cfg.id) || destructors.has(cfg.id)) {
+		throw Error("Potential resource leak, ID is duplicated");
+	} else {
+		constructors.set(cfg.id, cfg.constructor);
+		destructors.set(cfg.id, cfg.destructor);
+	}
+}
+
+export function deregister_resource(id: string) {
+	constructors.delete(id);
+	destructors.delete(id);
+}
+
