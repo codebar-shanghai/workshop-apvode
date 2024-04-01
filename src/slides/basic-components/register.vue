54a3e213917bbea11deb78d64453ad7c93274a42
IronBlood <aplshenyang@gmail.com>
2024-04-01 21:14:55 +0800

    register

---
+++ src/slides/basic-components/register.vue
@@ -0,0 +1,38 @@
+<template>
+	<section>
+		<h3>We can also build circuits to store data</h3>
+	</section>
+	<section>
+		<img src="../../assets/images/BFoCM-2-inverters-back-to-back.svg">
+		<table class="fragment" data-fragment-index="1">
+			<tr>
+				<td>A</td>
+				<td>B</td>
+				<td>C</td>
+			</tr>
+			<tr class="fragment" data-fragment-index="2">
+				<td>0</td>
+				<td>1</td>
+				<td>0</td>
+			</tr>
+			<tr class="fragment" data-fragment-index="3">
+				<td>1</td>
+				<td>0</td>
+				<td>1</td>
+			</tr>
+		</table>
+	</section>
+	<section>
+		<h3 class="fragment">D Latch</h3>
+		<img src="../../assets/images/BFoCM-D-Latch.svg">
+	</section>
+	<section>
+		<h3 class="fragment">S-R Latch</h3>
+		<img src="../../assets/images/BFoCM-S-R-Latch.svg">
+	</section>
+	<section>
+		<h3 class="fragment">4-bit Register</h3>
+		<img src="../../assets/images/BFoCM-4-bit-register.svg">
+	</section>
+</template>
+
