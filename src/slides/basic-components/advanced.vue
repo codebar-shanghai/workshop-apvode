8be2fdf85fa2557a5c697d55dc7c74b420e62d14
IronBlood <aplshenyang@gmail.com>
2024-04-01 22:38:15 +0800

    advanced components w/ snapshots from the video game turing complete

---
+++ src/slides/basic-components/advanced.vue
@@ -0,0 +1,21 @@
+<template>
+	<section>
+		<h3>Counter</h3>
+		<a href="https://store.steampowered.com/app/1444480/Turing_Complete/" target="_blank">
+			<img src="../../assets/images/TC-counting.png">
+		</a>
+	</section>
+	<section>
+		<h3>Decoder</h3>
+		<a href="https://store.steampowered.com/app/1444480/Turing_Complete/" target="_blank">
+			<img src="../../assets/images/TC-decoder.png">
+		</a>
+	</section>
+	<section>
+		<h3>ALU (Arithmetic Logic Unit)</h3>
+		<a href="https://store.steampowered.com/app/1444480/Turing_Complete/" target="_blank">
+			<img src="../../assets/images/TC-ALU.png">
+		</a>
+	</section>
+</template>
+
