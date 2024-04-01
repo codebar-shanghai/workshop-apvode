e7719afc3585eddb3d66eeb2c19a5ed2ae21c4dc
IronBlood <aplshenyang@gmail.com>
2024-04-01 16:25:13 +0800

    full adder

---
+++ src/slides/basic-components/full-adder.vue
@@ -0,0 +1,14 @@
+<template>
+	<section>
+		<h3>Full Adder</h3>
+		<div>
+			<div>
+				<img src="../../assets/images/BFoCM-full-adder.svg">
+			</div>
+			<div>
+				<img src="../../assets/images/BFoCM-full-adder-chain.svg">
+			</div>
+		</div>
+	</section>
+</template>
+
