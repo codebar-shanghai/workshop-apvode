1fc039a2f5e501736a0c28ad388cac929bba1826
IronBlood <aplshenyang@gmail.com>
2024-04-03 12:09:10 +0800

    break 1

---
+++ src/slides/break-1.vue
@@ -0,0 +1,12 @@
+<template>
+	<section>
+		<h3>Break</h3>
+		<ul>
+			<li>Signals, logic gates</li>
+			<li>von Neumann architecture</li>
+			<li class="fragment">Computers in early days</li>
+			<li class="fragment">Operating system</li>
+		</ul>
+	</section>
+</template>
+
