c5a645b04a4ccc3a7a1368f0fde07bd111ee3848
IronBlood <aplshenyang@gmail.com>
2024-04-08 11:26:58 +0800

    break 2

---
+++ src/components/problem-stack-5.vue
@@ -0,0 +1,14 @@
+<template>
+	<table class="problem-stack">
+		<tr class="ps-sw"><td>Problem</td></tr>
+		<tr class="ps-sw"><td>&nbsp;</td></tr>
+		<tr class="ps-sw"><td>Program/Language</td></tr>
+		<tr class="ps-sw"><td>Runtime System<br>(VM, OS, ...)</td></tr>
+		<tr class="ps-isa"><td>Instruction Set Architecture</td></tr>
+		<tr class="ps-hw"><td>Microarchitecture</td></tr>
+		<tr class="ps-hw"><td>Logic</td></tr>
+		<tr class="ps-hw"><td>Circuits</td></tr>
+		<tr class="ps-hw"><td>Electrons</td></tr>
+	</table>
+</template>
+
---
+++ src/slides/break-2.vue
@@ -0,0 +1,35 @@
+<template>
+	<section>
+		<h3>Break</h3>
+		<div class="column-2">
+			<div>
+				<ProblemStack5 />
+			</div>
+			<div>
+				<ul>
+					<li>ISA</li>
+					<li>UNIX and C</li>
+				</ul>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script setup lang="ts">
+import ProblemStack5 from "../components/problem-stack-5.vue";
+</script>
+
+<style scoped>
+.column-2 {
+	display: flex;
+}
+
+.column-2 > div {
+	width: 50%;
+}
+
+.column-2 table {
+	font-size: 28px;
+}
+</style>
+
