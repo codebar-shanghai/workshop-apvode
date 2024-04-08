93b25d72ba1e3f838eac609547b18b8a84539dfd
IronBlood <aplshenyang@gmail.com>
2024-04-08 12:07:52 +0800

    add highlight syntax for csharp/java/javascript/python

--- src/plugins/highlight.ts
+++ src/plugins/highlight.ts
@@ -2,13 +2,21 @@ import "highlight.js/styles/stackoverflow-light.css";
 import hljs          from "highlight.js/lib/core";
 import bash          from "highlight.js/lib/languages/bash";
 import c             from "highlight.js/lib/languages/c";
+import csharp        from "highlight.js/lib/languages/csharp";
+import java          from "highlight.js/lib/languages/java";
+import javascript    from "highlight.js/lib/languages/javascript";
 import plaintext     from "highlight.js/lib/languages/plaintext";
+import python        from "highlight.js/lib/languages/python";
 import x86asm        from "highlight.js/lib/languages/x86asm";
 import hljsVuePlugin from "@highlightjs/vue-plugin";
 
 hljs.registerLanguage("bash", bash);
 hljs.registerLanguage("c", c);
+hljs.registerLanguage("csharp", csharp);
+hljs.registerLanguage("java",java);
+hljs.registerLanguage("javascript",javascript);
 hljs.registerLanguage("plaintext", plaintext);
+hljs.registerLanguage("python", python);
 hljs.registerLanguage("x86asm", x86asm);
 
 export default hljsVuePlugin;
