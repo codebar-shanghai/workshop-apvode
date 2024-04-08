682c86648505282633ce56d0cfa3e9d894cd5616
IronBlood <aplshenyang@gmail.com>
2024-04-08 11:28:47 +0800

    terminals

---
+++ src/slides/terminal-shell-emulator.vue
@@ -0,0 +1,74 @@
+<template>
+	<section>
+		<section>
+			<ul>
+				<li>Terminals?</li>
+				<li>Consoles?</li>
+				<li>Shells?</li>
+				<li>Terminal Emulators?</li>
+			</ul>
+		</section>
+		<section>
+			<h3>Terminals</h3>
+			<a href="https://www.flickr.com/photos/16057786@N00/1678948976">
+				<img src="../assets/images/1678948976_14529e3278_c.jpg" />
+			</a>
+		</section>
+		<section>
+			<h3>Shells</h3>
+			<div>The kernel of an operating system manages hardwares.</div>
+			<div>A shell provides an interface to interact with the kernel indirectly.</div>
+		</section>
+		<section>
+			<h3>Notable Shells</h3>
+			<ul>
+				<li>Thompson shell - 1971, Ken Thompson</li>
+				<li>Bourne shell (sh) - 1979, Stephen Bourne</li>
+				<li>Bourne-Again SHell (bash) - 1989, Brian Fox</li>
+				<li>C shell (csh) - 1978, Bill Joy (BSD, vi, Sun Microsystems, Java, SPARC, Solaris, ...)</li>
+				<li>Powershell - 2006, Microsoft</li>
+				<li>Z shell (zsh) - 1990, Paul Falstad</li>
+			</ul>
+		</section>
+		<section>
+			<h3>Consoles</h3>
+			<a href="https://www.flickr.com/photos/ajmexico/3281139507/">
+				<img src="../assets/images/3281139507_f56091fa84_z.jpg" />
+			</a>
+		</section>
+		<section>
+			<a href="https://en.wikipedia.org/wiki/VT100">
+				<img src="../assets/images/DEC_VT100_terminal.jpg" />
+			</a>
+		</section>
+		<section>
+			<h3>Pseudoterminal</h3>
+			<ul>
+				<li>Remote logins (telnet, ssh)</li>
+				<li>Hardware terminals are less and less</li>
+			</ul>
+		</section>
+		<section>
+			<img src="../assets/images/image_e3c2af63-3682-44c6-89b3-4c7d09f0fcf4.png" />
+		</section>
+		<section>
+			<h3>Notable Terminal Emulators</h3>
+			<ul>
+				<li><a href="https://hyper.is/">Hyper</a></li>
+				<li><a href="https://conemu.github.io/">ConEmu</a></li>
+				<li><a href="https://en.wikipedia.org/wiki/PuTTY">PuTTY</a></li>
+				<li><a href="https://apps.microsoft.com/detail/9n0dx20hk701">Windows Terminal</a></li>
+				<li><a href="https://alacritty.org/">Alacritty</a></li>
+			</ul>
+			<div>Yet another GUI application</div>
+			<div>Take a try or watch tutorials</div>
+		</section>
+	</section>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+</style>
+
