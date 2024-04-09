3bfeb18d3f7d3c68147b12856e863b04ca72e521
IronBlood <aplshenyang@gmail.com>
2024-04-09 22:48:55 +0800

    build: :heavy_minus_sign: remove vite-svg-loader

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,12 @@ import {
 	splitVendorChunkPlugin,
 } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		vue(),
 		splitVendorChunkPlugin(),
-		svgLoader(),
 	],
 	server: {
 		host: true,
