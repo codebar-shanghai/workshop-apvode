e52db8167c6b9e18153dfba49b31a363ab74a69c
IronBlood <aplshenyang@gmail.com>
2024-04-12 23:36:37 +0800

    fix: :bug: memory leak in example code

--- src/assets/code/fd.c
+++ src/assets/code/fd.c
@@ -1,10 +1,12 @@
-#include <stdio.h> // for printf()
-#include <fcntl.h> // for open() and O_RDWR
+#include <stdio.h>  // for printf()
+#include <fcntl.h>  // for open() and O_RDWR
+#include <unistd.h> // for close()
 
 int main() {
 	// Attempt to open /dev/null
 	int fd = open("/dev/null", O_RDWR);
 	printf("The file discriptor is %d\n", fd);
+	close(fd); // otherwise memory leak
 	return 0;
 }
 
