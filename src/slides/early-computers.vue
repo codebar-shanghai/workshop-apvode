356d9892df358c1dc15fddaa105c861dabb245c6
IronBlood <aplshenyang@gmail.com>
2024-04-12 17:01:49 +0800

    style: :dizzy: add animation

--- src/slides/early-computers.vue
+++ src/slides/early-computers.vue
@@ -3,11 +3,11 @@
 		<section>
 			<h3>Computers in Early Days</h3>
 			<ul>
-				<li>1940 - <a href="https://en.wikipedia.org/wiki/Bombe">Bombe</a></li>
-				<li>1945 - <a href="https://en.wikipedia.org/wiki/ENIAC">ENIAC</a></li>
-				<li>1951 - <a href="https://en.wikipedia.org/wiki/UNIVAC">UNIVAC</a></li>
-				<li>1952 - <a href="https://en.wikipedia.org/wiki/IBM_701">IBM 701</a> (30 were sold in 1953-54)</li>
-				<li>1954 - <a href="https://en.wikipedia.org/wiki/IBM_650">IBM 650</a> (120 were sold in 1954, and more than 750 orders)</li>
+				<li class="fragment">1940 - <a href="https://en.wikipedia.org/wiki/Bombe">Bombe</a></li>
+				<li class="fragment">1945 - <a href="https://en.wikipedia.org/wiki/ENIAC">ENIAC</a></li>
+				<li class="fragment">1951 - <a href="https://en.wikipedia.org/wiki/UNIVAC">UNIVAC</a></li>
+				<li class="fragment">1952 - <a href="https://en.wikipedia.org/wiki/IBM_701">IBM 701</a> (30 were sold in 1953-54)</li>
+				<li class="fragment">1954 - <a href="https://en.wikipedia.org/wiki/IBM_650">IBM 650</a> (120 were sold in 1954, and more than 750 orders)</li>
 			</ul>
 		</section>
 		<section>
@@ -17,9 +17,9 @@
 		<section>
 			<h3>Programmer's view of IBM 650</h3>
 			<ul>
-				<li>A machine with 44 instructions</li>
-				<li>Instruction: <code>60 1234 1009</code>
-					<ul>
+				<li class="fragment">A machine with 44 instructions</li>
+				<li class="fragment">Instruction: <code>60 1234 1009</code>
+					<ul class="fragment">
 						<li>Load the contents of location <code>1234</code> into the <span class="italic">distribution</span>; put it also into the <span class="italic">upper accumulator</span>; set the <span class="italic">lower accumulator</span> to zero; and then go to the location <code>1009</code> for the next instruction.</li>
 					</ul>
 				</li>
@@ -28,9 +28,9 @@
 		<section>
 			<h3>Computers in mid 50's</h3>
 			<ul>
-				<li>Hardware was super expensive</li>
-				<li>Stores were small (1000 words)</li>
-				<li>Programmer's view of the machine was inseparable from the actual hardware implementation</li>
+				<li class="fragment">Hardware was super expensive</li>
+				<li class="fragment">Stores were small (1000 words)</li>
+				<li class="fragment">Programmer's view of the machine was inseparable from the actual hardware implementation</li>
 			</ul>
 		</section>
 		<section>
@@ -53,15 +53,15 @@
 				<li class="fragment">For commercial or data processing use: 702, 705, ...</li>
 				<li class="fragment">Mid-range business small mainframe: 1400 series</li>
 			</ul>
-			<h4>They had completely different instruction sets, I/O system, storage, softwares, etc</h4>
+			<h4 class="fragment">They had completely different instruction sets, I/O system, storage, softwares, etc</h4>
 		</section>
 		<section>
 			<h3>IBM System/360</h3>
 			<ul>
-				<li>Announced on April 7, 1964. Delivered between 1965 and 1978.</li>
-				<li>Introduced a new layer of <span class="strong">abstraction</span> now well known as <span class="strong">ISA</span> (instruction set architecture).</li>
-				<li>ISA specifies <span class="strong">what</span> hardware provides, not <span class="strong">how</span> it's implemented.</li>
-				<li>It was revolutionary and extraodinarily challenging, see <a href="https://en.wikipedia.org/wiki/The_Mythical_Man-Month">The Mythical Man-Month / 人月神话</a></li>
+				<li class="fragment">Announced on April 7, 1964. Delivered between 1965 and 1978.</li>
+				<li class="fragment">Introduced a new layer of <span class="strong">abstraction</span> now well known as <span class="text-red-500">ISA</span> (instruction set architecture).</li>
+				<li class="fragment">ISA specifies <span class="text-red-500 italic">what</span> hardware provides, not <span class="text-red-500 italic">how</span> it's implemented.</li>
+				<li class="fragment">It was revolutionary and extraodinarily challenging, see <a href="https://en.wikipedia.org/wiki/The_Mythical_Man-Month">The Mythical Man-Month / 人月神话</a></li>
 			</ul>
 		</section>
 		<section>
@@ -97,8 +97,8 @@
 		<section>
 			<h3>The Tradeoffs of ISA</h3>
 			<ul>
-				<li>Enables fast innovation in hardware w/ no need to change the software</li>
-				<li>Commercially successful ISAs last for decades</li>
+				<li class="fragment">Enables fast innovation in hardware w/ no need to change the software</li>
+				<li class="fragment">Commercially successful ISAs last for decades</li>
 			</ul>
 		</section>
 	</section>
