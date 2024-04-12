bb382288dfb4168d6793c7d90e3cfacf0dd24db8
IronBlood <aplshenyang@gmail.com>
2024-04-12 11:54:42 +0800

    style: :lipstick: adjust layout

--- src/slides/basic-components/advanced.vue
+++ src/slides/basic-components/advanced.vue
@@ -1,21 +1,37 @@
 <template>
 	<section>
 		<h3>Counter</h3>
-		<a href="https://store.steampowered.com/app/1444480/Turing_Complete/" target="_blank">
-			<img src="../../assets/images/TC-counting.png">
-		</a>
+		<div class="flex justify-center">
+			<a href="https://store.steampowered.com/app/1444480/Turing_Complete/" target="_blank">
+				<img src="../../assets/images/TC-counting.png" />
+			</a>
+		</div>
 	</section>
 	<section>
 		<h3>Decoder</h3>
-		<a href="https://store.steampowered.com/app/1444480/Turing_Complete/" target="_blank">
-			<img src="../../assets/images/TC-decoder.png">
-		</a>
+		<div class="flex justify-center">
+			<a href="https://store.steampowered.com/app/1444480/Turing_Complete/" target="_blank">
+				<img src="../../assets/images/TC-decoder.png" />
+			</a>
+		</div>
 	</section>
 	<section>
 		<h3>ALU (Arithmetic Logic Unit)</h3>
-		<a href="https://store.steampowered.com/app/1444480/Turing_Complete/" target="_blank">
-			<img src="../../assets/images/TC-ALU.png">
-		</a>
+		<div class="flex justify-center">
+			<a href="https://store.steampowered.com/app/1444480/Turing_Complete/" target="_blank">
+				<img src="../../assets/images/TC-ALU.png" class="scale-75 m-0" />
+			</a>
+		</div>
 	</section>
 </template>
+
+<style scoped>
+.reveal h3 {
+	margin-bottom: 10px;
+}
+.reveal img {
+	margin-top: 0px;
+	margin-bottom: 0px;
+}
+</style>
 
