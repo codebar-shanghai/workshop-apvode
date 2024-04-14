080b3fab73d9cbd827cfcd2729f7819cccb7ca47
IronBlood <aplshenyang@gmail.com>
2024-04-14 23:00:16 +0800

    ci: :construction_worker: fix base

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,11 @@ import {
 } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const repoName = process.env.VITE_REPO_NAME || '/';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: repoName,
 	plugins: [
 		vue(),
 		splitVendorChunkPlugin(),
